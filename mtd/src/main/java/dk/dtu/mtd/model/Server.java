05687af51e2791ed0486dd9371627db78d3fb276
Pinkymus <katinkaspangtoft@gmail.com>
2024-01-04 14:25:16 +0100

    Get correct ip

--- mtd/src/main/java/dk/dtu/mtd/model/Server.java
+++ mtd/src/main/java/dk/dtu/mtd/model/Server.java
@@ -1,14 +1,27 @@
 package dk.dtu.mtd.model;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.jspace.*;
 
 public class Server {
 
     SpaceRepository server;
+    String ip;
 
     public Server(){
         server = new SpaceRepository();
-
         server.add("lobby", new SequentialSpace());
+        try {
+            ip = (InetAddress.getLocalHost().getHostAddress()).trim();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        server.addGate("tcp://" + ip + ":37331/?keep");
+    }
+
+    public static void main(String[] args) {
+        Server ser = new Server();
     }
 }
