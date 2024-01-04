04ea88b407c24efe80efb14ae2aad444eb2e8ebb
Christian <cvedelpetersen@gmail.com>
2024-01-04 15:09:13 +0100

    Moved controller file

--- mtd/src/main/java/dk/dtu/mtd/controller/Controller.java
+++ mtd/src/main/java/dk/dtu/mtd/controller/Controller.java
@@ -5,13 +5,14 @@ import org.jspace.ActualField;
 import dk.dtu.mtd.model.Client;
 
 public class Controller {
-    static Client client = new Client("10.209.248.191");;
+    static Client client = new Client("10.209.240.41");;
 
 
     public static void joinLobby() {
         try {
             client.lobby.put("hey");
             client.lobby.get(new ActualField("hey back"));
+            System.out.println("Got hey back");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
