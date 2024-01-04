f2bc3d83df345155289c104131b6f1a6dae74552
Pinkymus <katinkaspangtoft@gmail.com>
2024-01-04 15:45:33 +0100

    Create game in server

--- mtd/src/main/java/dk/dtu/mtd/model/Server.java
+++ mtd/src/main/java/dk/dtu/mtd/model/Server.java
@@ -5,14 +5,17 @@ import java.net.UnknownHostException;
 
 import org.jspace.*;
 
+import dk.dtu.mtd.model.game.Game;
+
 public class Server {
 
     SpaceRepository server;
     Space lobby;
     String ip;
-    int IDrequest = 0; 
+    int IDrequest = 0;
+    int IDgame = 0;
 
-    public Server(){
+    public Server() {
         server = new SpaceRepository();
         lobby = new SequentialSpace();
         server.add("lobby", lobby);
@@ -25,23 +28,43 @@ public class Server {
         System.out.println("Server ip: " + ip);
     }
 
-    public void launch(){
-        while(true){
+    public void launch() {
+        while (true) {
             try {
-
-                Object[] request = lobby.get(new ActualField("request"), new FormalField(String.class), new FormalField(Integer.class));
-                
-                if((request[1].toString()).equals("id")){
-                    lobby.put("id", IDrequest);
-                    IDrequest++;
-                } else if ((request[1].toString()).equals("game")){
-                    
-                }
+                handleRequest(lobby.get(new ActualField("request"), new FormalField(String.class),
+                        new FormalField(Integer.class)));
 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    void handleRequest(Object[] request) {
+        try {
+            if ((request[1].toString()).equals("id")) {
+                lobby.put("id", IDrequest);
+                IDrequest++;
+            } else if ((request[1].toString()).equals("game")) {
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void createGame(int playerID1, int playerID2) {
+        Game newGame = new Game(IDgame);
+
+        server.add(("game" + IDgame), newGame.space);
+        new Thread(newGame).start();
+        try {
+            lobby.put("game", playerID1, newGame.id);
+            lobby.put("game", playerID2, newGame.id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        IDgame++;
     }
 
     public static void main(String[] args) {
