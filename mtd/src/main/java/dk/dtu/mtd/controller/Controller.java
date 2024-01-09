be984703448d3c45cc44c393bbd608aeb0d1eb28
ChristianWantzin <chr.wantzin@gmail.com>
2024-01-09 12:43:13 +0100

    Wave manager udkast ready

--- mtd/src/main/java/dk/dtu/mtd/controller/Controller.java
+++ mtd/src/main/java/dk/dtu/mtd/controller/Controller.java
@@ -7,7 +7,7 @@ import java.net.UnknownHostException;
 import java.util.LinkedList;
 
 import dk.dtu.mtd.model.Client;
-import dk.dtu.mtd.model.game.WaveManager;
+//import dk.dtu.mtd.model.game.WaveManager;
 import dk.dtu.mtd.view.GameGui;
 import dk.dtu.mtd.view.Gui;
 import javafx.application.Platform;
@@ -16,8 +16,6 @@ public class Controller {
     public static Controller controller;
     private static GUIMonitior guiMonitior;
     private static Thread guiThread;
-    private static WaveManager waveManager;
-    //private static Thread waveManagerThread;
     private static Client client = new Client();
 
 
@@ -37,11 +35,6 @@ public class Controller {
         guiThread = new Thread(guiMonitior);
         guiMonitior.playing = true;
         guiThread.start();
-
-        /*waveManager = new WaveManager(10, 15);
-        waveManagerThread = new Thread(waveManager);
-        waveManagerThread.start();*/
-
         Platform.runLater(() -> {
             Gui.game();
         });
@@ -49,11 +42,6 @@ public class Controller {
     }
 
     public static void exitGame() {
-        /*if (waveManager != null) {
-            waveManager.stop();
-            waveManagerThread.interrupt();
-        }*/
-
         guiMonitior.playing = false;
         Gui.closeGame();
         client.exitGame();
@@ -129,9 +117,6 @@ class GUIMonitior implements Runnable {
 
                     });
                 } else if (update[1].toString().equals("wave")) {
-                    /*WaveManager waveManager = new WaveManager(10, 15);
-                    Thread waveManagerThread = new Thread(waveManager);
-                    waveManagerThread.start();*/
                     // make apropriate gui calls to display wave
 
                 } else if (update[1].toString().equals("enemyUpdate")) {
