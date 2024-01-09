edc57a7e3b239a0d7e3828114facdc99fd302f64
Pinkymus <katinkaspangtoft@gmail.com>
2024-01-09 10:04:31 +0100

    Gui class created

--- mtd/src/main/java/dk/dtu/mtd/view/GameWaveGui.java
+++ mtd/src/main/java/dk/dtu/mtd/view/GameWaveGui.java
@@ -1,5 +1,32 @@
 package dk.dtu.mtd.view;
 
-public class GameWaveGui {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class GameWaveGui extends Pane{
+
+    public GameWaveGui(){
+
+    }
+
+
+
+    ImageView enemy(){
+        Image image = new Image("/dk/dtu/mtd/skelly.gif", 100, 0, true, false);
+        ImageView imageView = new ImageView(image);
+
+        Timeline timeline = new Timeline(
+            new KeyFrame(Duration.ZERO, new KeyValue(imageView.imageProperty() , 1))
+        );
+
+
+        return imageView;
+    }
     
 }
