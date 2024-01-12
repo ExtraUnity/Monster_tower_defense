a299c6b8fbc6902c02a088e28165917af5d6e171
Tobias Martinsen <t.f.martinsen@gmail.com>
2024-01-12 14:35:50 +0100

    made aoe tower

---
+++ mtd/src/main/java/dk/dtu/mtd/model/game/AoeTower.java
@@ -0,0 +1,42 @@
+package dk.dtu.mtd.model.game;
+
+import java.util.List;
+
+public class AoeTower extends Tower {
+
+    public AoeTower(int x, int y, int towerId, int playerId, GameTicker gameTicker) {
+        this.x = x;
+        this.y = y;
+        this.playerId = playerId;
+        this.type = "aoeTower";
+        //this.gameTicker = gameTicker;
+        this.lastShot = -1;
+        this.towerId = towerId;
+        radius = 200;
+        damage = 3;
+        size = 100;
+        fireRate = 70;
+        towerCost = 15;
+        upgradeCost = 5;
+    }
+
+    @Override
+    public void shoot(List<Enemy> enemies, Game game) {
+        if(this.lastShot == -1) {
+            this.lastShot = game.gameTicker.gameTick;
+        }
+        int deltaTick = game.gameTicker.gameTick - lastShot;
+
+        if (deltaTick > fireRate) {
+            for (int i = 0; i < enemies.size(); i++) {
+                if (inRange(enemies.get(i)) && !enemies.get(i).isDead()) {
+                    enemies.get(i).takeDamage(damage, playerId, game);
+                    lastShot = game.gameTicker.gameTick;
+                    deltaTick = 0;
+                }
+            }
+        }
+    }
+    
+
+}
