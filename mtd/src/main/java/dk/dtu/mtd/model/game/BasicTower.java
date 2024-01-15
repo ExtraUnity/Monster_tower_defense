7959de4fdb7bf68ad0cece185892af14f5bdcf58
Christian <cvedelpetersen@gmail.com>
2024-01-15 11:15:48 +0100

    removed printline

--- mtd/src/main/java/dk/dtu/mtd/model/game/BasicTower.java
+++ mtd/src/main/java/dk/dtu/mtd/model/game/BasicTower.java
@@ -29,7 +29,6 @@ public class BasicTower extends Tower {
         if (deltaTick > fireRate) {
             for (int i = 0; i < enemies.size(); i++) {
                 if (inRange(enemies.get(i)) && !enemies.get(i).isDead()) {
-                    System.out.println("Player " + playerId + " killed enemy at: " + enemies.get(i).getX() + " " + enemies.get(i).getY() + " with tower at: " + x + " " + y);
                     enemies.get(i).takeDamage(damage, playerId, game);
                     lastShot = game.gameTicker.gameTick;
                     deltaTick = 0;
